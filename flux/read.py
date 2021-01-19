db5fdea290e68a136a44a0e097db86e4b182d25c
Olivier Peltre <opeltre@gmail.com>
2021-01-19 13:15:43 +0100

    move text parsing to read.py

--- flux/read.py
+++ flux/read.py
@@ -1,22 +1,17 @@
 import re
 import csv
-import numpy as np
 from os import path
 from glob import glob
 
 from dict import Dict
 
-Levels = Dict({})
-with open("levels.csv") as f:
-    for level in csv.DictReader(f):
-        d = Dict(level)
-        d.map_(lambda v, k: re.search(r'\s*(.*?)\s*$', v).group(1))
-        key = d.pop('key')
-        Levels[key] = d
-
 """
-Type declarations 
------------------
+    This module provides basic utilities to parse raw exam directories. 
+
+    Keep this file for text and regex parsing only. 
+
+    Types:
+    ------
 
     flux : {
         type    : 'A' | 'V' | 'CSF',
@@ -28,24 +23,35 @@ Type declarations
         name    : string
     }
 
-    level   : 'aqueduc'     | 'cid_cereb'   | 'cid_cervi'   
-            | 'citer-prep'  | 'cig_cereb'   | 'cig_cervi'
-            | 'sinus-d'     | 'coro_sinus_d'| 'coro_sinus_lat_d'
-            | 'sinus-s'     | 'coro_sinus_s'| 'coro_sinus_lat_g'
-            | 'jugul-d'     | 'verteb-d'    | 'verteb-g'
-            | 'v4'          | 'tb'          | 'c2_c3'       
+    level   : 'aqueduc'                 | 'v4'      
+            | 'citer-prep'              | 'c2_c3'
+            -   -   -   -   -   -   -   -   -   -   -   -
+            | 'ci{d,g}_cereb'           | 'verteb-{d,g}'  
+            | 'ci{d,g}_cervi'           | 'tb'
+            -   -   -   -   -   -   -   -   -   -   -   -   
+            | 'sinus-s'                 | 'coro_sinus_s'
+            | 'sinus-d'                 | 'coro_sinus_d'
+            | 'coro_sinus_lat_{d,g}'    | 'jugul-{d,g}'   
 
-    exam : { level > flux }
+
+    exam : str -> { level > flux }
 """
+
+Levels = Dict({})
+with open("levels.csv") as f:
+    for level in csv.DictReader(f):
+        d = Dict(level)
+        d.map_(lambda v, k: re.search(r'\s*(.*?)\s*$', v).group(1))
+        key = d.pop('key')
+        Levels[key] = d
+
 
 def Exam (dirname, *patterns):
     """ Parse all exam files (or only those matching patterns). 
 
-            Exam : str -> { flux } 
-
         Example:
         --------
-        >>> exam = Exam('I', 'coro_sinus*', 'aqueduc')
+        >>> exam = Exam('I', 'sinus*', 'aqueduc')
     """ 
     patterns = ['*'] if not len(patterns) else patterns
     files = []
@@ -88,8 +94,6 @@ def Flux (name):
                 env = 'volume'
             elif re.search('Axe des Temps', line):
                 env = 'time'
-    
-    curves.map_(lambda v, k: np.array(v))
     
     key = re.search(r'/(.*)\.txt$', name).group(1)
     curves.update(Levels[key])
