c69f598b0f5747c6933ab9e8271e247d0d2933ce
Olivier Peltre <opeltre@gmail.com>
2021-12-08 10:51:12 +0100

    clean datasets/

--- revert/transforms/sample.py
+++ revert/transforms/sample.py
@@ -3,26 +3,24 @@ import math
 from .diff import diff
 
 def resample(N):
-    def runResample(t):
-        M = t.shape[0]
-        x = torch.linspace(0, 1, N)
-        out = torch.zeros((N,))
-        dt = diff(t)
-        cub = [cubic(t[i], t[i + 1], dt[i], dt[i + 1]) \
-               for i in range(M - 1)]
-        for i, xi in enumerate(x):
-            y = xi * (M - 1)
-            j = min(math.floor(y), M - 2)
-            dy = y - j
-            out[i] = sum(cub[j][k] * dy ** k for k in range(4))
-        return out
+
+    def runResample(x):
+        M   = x.shape[0]
+        t  = torch.linspace(0, 1, N) * (M - 1)
+        t0 = torch.min(t.floor(), torch.tensor(M - 2)[None])
+        dt = torch.stack([(t - t0) ** k for k in range(4)])
+        cub = cubic(x, diff(x)).index_select(1, t0.long())
+        return (cub * dt).sum(dim=0)
+
     return runResample
 
-def cubic(x0, x1, v0, v1):
+def cubic(x, dx):
+    x0, x1  = x[:-1],  x[1:]
+    v0 , v1 = dx[:-1], dx[1:]
     a = 6 * (x1 - x0 - (v0 + v1) / 2.)
     a2 = (a + v1 - v0) / 2. 
     a3 = - a / 3  
-    return torch.tensor([x0, v0, a2, a3])
+    return torch.stack([x0, v0, a2, a3])
 
 def repeat(n): 
     return lambda t: torch.cat([t] * n)
