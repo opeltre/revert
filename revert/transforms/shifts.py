422820aa8c53501557aded3184b5cbf121e39d71
b-brebion <benoit_brebion@ens.univ-artois.fr>
2022-04-14 15:22:50 +0200

    Adding shift_one() to shifts.py

--- revert/transforms/shifts.py
+++ revert/transforms/shifts.py
@@ -1,3 +1,5 @@
+import torch
+
 def shift_all (stdev):
     def run_shift(x):
         N = len(x)
@@ -19,3 +21,24 @@ def shift_all (stdev):
         return x_prime, y
     
     return run_shift
+
+def shift_one(x):
+    """
+        Input:
+            - x corresponds to a list of channels
+        Outputs:
+            - x corresponds to the same list of channels (with one of them shifted)
+            - y corresponds to the list of shifts
+    """
+    if x.dim() == 2:
+        y = torch.tensor([0 for i in range(len(x))])
+        nb_chan = torch.randint(0, len(x), (1,))
+        half = x.shape[-1] // 2
+        shift = torch.randint(-half, half+1, (1,)).item()
+        x[nb_chan] = x[nb_chan].roll(-shift, -1)
+        y[nb_chan] = shift
+        return x, y
+    elif x.dim() == 3:
+        return torch.stack([shift_one(xi) for xi in x])
+    else:
+        raise TypeError("x must be of shape (N, Nc, Npts) or (Nc, Npts)")
