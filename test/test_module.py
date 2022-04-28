26a50d6f93b23123c5b0e943549fdec0355d9efa
Olivier Peltre <opeltre@gmail.com>
2022-04-28 18:19:58 +0200

    add test_loss for piped modules

--- test/test_module.py
+++ test/test_module.py
@@ -5,6 +5,7 @@ from revert.models import ConvNet, Pipe
 
 N = 20 
 x = torch.randn([N, 6, 32])
+y = torch.randn([N, 24, 8])
 
 f1 = ConvNet([[32, 6, 4],
               [16, 12, 1]])
@@ -12,10 +13,19 @@ f1 = ConvNet([[32, 6, 4],
 f2 = ConvNet([[16, 12, 4],
               [8,  24, 1]])
 
+
 class TestConv(test.TestCase):
 
-    def test_pipe (self): 
+    def test_pipe(self): 
         model = Pipe(f1, f2)
         result = tuple(model(x).shape)
         expect = (N, 24, 8)
         self.assertEqual(expect, result)
+
+    def test_loss(self):
+        f2.loss = lambda out, tgt : ((out - tgt)**2).sum()
+        model = Pipe(f1, f2)
+        loss = model.loss_on(x, y)
+        self.assertTrue(loss.shape == ())
+
+        
