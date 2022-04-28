8d28357136caad03cadd252015b0c0068a58613f
Olivier Peltre <opeltre@gmail.com>
2022-04-28 12:55:39 +0200

    improve tests : shuffle/unshuffle on random input

--- test/test_shuffle.py
+++ test/test_shuffle.py
@@ -3,41 +3,33 @@ from revert.transforms import shuffle_all, shuffle_two, unshuffle
 import test
 import torch
 
-# shuffle_all() and shuffle_two
-N = 500
-x = torch.arange(32).repeat(6).repeat(N).view([N, 6 ,32])
-
-x2 = []
-for i in range(N) :  
-    x2.append([])
-    for j in range(6)  :
-        x2[i].append([j for _ in range(32)])
-x2 = torch.tensor(x2)
+N, Nc, Npts = 20, 6, 32
 
 class TestShift(test.TestCase):
 
     # Test with that type of list : [[0...32], [0...32], ... ,[0...32]]
-    def test_unshuffe_two(self):
+    def test_unshuffle_two(self):
+        x = torch.randn([N, Nc, Npts]) 
         one, y = shuffle_two(x)
         result = unshuffle(one,y)
         expect = x
         self.assertClose(expect, result)
-
-        
             
     # Test with that type of list : [[0...0], [1...1], ... ,[5...5]]
 
     def test_unshuffle_all(self):
+        x2 = torch.randn([N, Nc, Npts])
         two, y2 = shuffle_all(x2)
         result = unshuffle(two, y2)
         expect = x2
         self.assertClose(expect, result)
         
-    # Test with that type of list : [[0...32], [0...32], ... ,[0...32]] with N = 1
+    # Test with [Nc, Npts] tensor
     
     def test_unshuffle_all(self):
-        one, y = shuffle_all(x[0])
+        x = torch.randn([Nc, Npts])
+        one, y = shuffle_all(x)
         result = unshuffle(one, y)
-        expect = x[0]
+        expect = x
         self.assertClose(expect, result)
 
