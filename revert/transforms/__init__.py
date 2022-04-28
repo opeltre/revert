7281816bfb4ad93eb397b25c2f32e4bf9c07fab9
Adrien Hannon <adrien_hannon@ens.univ-artois.fr>
2022-04-28 10:53:53 +0200

    Add unshuffle to __init__.py

--- revert/transforms/__init__.py
+++ revert/transforms/__init__.py
@@ -9,3 +9,4 @@ from .diff   import diff, jet, laplacian
 from .scholkmann import scholkmann, diff_scalogram, Troughs
 from .segment import segment, mask_center
 from .shifts import shift_all, shift_one, unshift, mod
+from .shuffle import shuffle_all, shuffle_two, unshuffle
