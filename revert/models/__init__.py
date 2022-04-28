936e00ab95afca9af2ac427c3610bae6cc1b28f0
Olivier Peltre <opeltre@gmail.com>
2022-04-28 13:14:12 +0200

    update models/__init__.py

--- revert/models/__init__.py
+++ revert/models/__init__.py
@@ -1,6 +1,6 @@
 from .neural_decomposition import ND
 from .barlow_twins  import BarlowTwins, cross_correlation, norm2
 from .conv          import ConvNet
-from .module        import Module
+from .module        import Module, Pipe
 from .geometry      import tsne, mdse, pca
 from .kmeans        import KMeans
