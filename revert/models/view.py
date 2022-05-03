d1a9f65bed9050e88167bf4b74e6176a3b95a92a
Olivier Peltre <opeltre@gmail.com>
2022-05-03 12:27:16 +0200

    allow arbitrary batch dimension for View

--- revert/models/view.py
+++ revert/models/view.py
@@ -3,13 +3,21 @@ import torch
 from revert.models import Module
 
 class View(Module):
+    """ 
+    Module reshaping its input, preserving batch dimension. 
+    """
+
     def __init__(self, shape):
+        """ 
+        Create module. 
+
+        Input: 
+            - shape : sizes after the batch dimension.
+        """
         super().__init__()
         self.shape = shape
 
     def forward(self, input):
-        '''
-            Reshaping the input
-        '''
-        out = input.view(self.shape)
+        """ Reshape the input. """
+        out = input.view([-1, *self.shape])
         return out
