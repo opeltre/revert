0de3235d0d881bcdda34b8430ef13e3c60ea3812
Olivier Peltre <opeltre@gmail.com>
2022-05-03 17:58:12 +0200

    clean up cli_tools.py

--- experiments/cli_tools.py
+++ experiments/cli_tools.py
@@ -2,74 +2,96 @@ import argparse
 import datetime
 import os
 
-def arg_parser(prefix='convnet'):
+def arg_parser(prefix='module'):
+    """ 
+    Return an ArgumentParser object with input, output and data arguments.
+    """
     parser = argparse.ArgumentParser()
 
-    if prefix == 'convnet':
-        parser.add_argument('--input', '-i', type=str, nargs=1, help="Should target an existing '.pt' file. If 'path' is not absolute, the model will be searched using the following path : $REVERT_MODELS/path (make sure to define the '$REVERT_MODELS' environment variable first). Model randomly initialized by default if '--input' is not called.", metavar="path_to_input_file_state")
-        parser.add_argument('--output', '-o', type=str, nargs=1, help="Should point to the folder where you want to save the model state (filename is automatically generated: 'convnet-monthday-num'). If 'path' is relative, the model will be saved in : $REVERT_MODELS/path/runs/model_name (make sure to define the '$REVERT_MODELS' environment variable first). Creates tensorboard traces in '$REVERT_LOGS/path'.", metavar="path_to_output_folder")
+    parser.add_argument('--input', '-i', type=str, metavar="path_to_input_file_state",
+        help="""
+        Initial model state path, should target an existing '.pt' file (default=None).  
+        If 'path' relative and the '$REVERT_MODELS' environment variable 
+        is defined, the state file will be looked inside this folder. 
+        Model is randomly initialized otherwise.
+        """)
+    parser.add_argument('--output', '-o', type=str, metavar="path_to_output_folder",
+        help="""
+        Path where final model state should be saved (default=None). 
+        If 'path' relative and the '$REVERT_MODELS' environment variable 
+        is defined, the state file will be saved inside this folder, 
+        with tensorboard traces saved in '$REVERT_LOGS/path'.
+        File name automatically generated if not given. 
+        """)
     
     return parser
 
-def generate_filename(path, prefix='convnet'):
-    date = datetime.datetime.now()
-    day_num = str(date.day)
-    month_num = str(date.month)
-    datetime_object = datetime.datetime.strptime(month_num, "%m")
-    month_name = datetime_object.strftime("%b").lower()
-    
+
+def try_envdir (name): 
+    """ Return $name or $PWD """
+    if name in os.environ and os.path.isdir(os.environ[name]):
+        return os.environ[name]
+    print(f"warning: ${name} is undefined, falling back to $PWD\n")
+    return os.getcwd()
+
+
+def generate_filename(dirname, prefix='module', use_date=True):
+    """ 
+    Generate a filename index from module prefix and date string.
+    """
+    # time info
+    if use_date: 
+        date = datetime.datetime.now()
+        day_num = str(date.day)
+        month_num = str(date.month)
+        datetime_object = datetime.datetime.strptime(month_num, "%m")
+        month_name = datetime_object.strftime("%b").lower()
+        daykey = f"{month_name[:3]}{day_num}"
+    # prefix 
+    basename = f"{prefix}-{daykey}" if use_date else prefix
+    # next slot from prefix
     num = 1
-    while os.path.exists(os.path.join(path, "{}-{}{}-{}.pt".format(prefix, month_name, day_num, num))):
+    while os.path.exists(os.path.join(dirname, f"{basename}-{num}.pt")):
         num += 1
-        
-    return "{}-{}{}-{}".format(prefix, month_name, day_num, num)
+    return os.path.join(dirname, f"{basename}-{num}.pt")
 
-def arg_verifier(parser, prefix='convnet'):
+
+def read_args(parser, prefix='module'):
+    """ 
+    Return argument namespace after some processing. 
+    """
+
     args = parser.parse_args()
+    models_dir = try_envdir('REVERT_MODELS')
+    logs_dir   = try_envdir('REVERT_LOGS')
 
-    if prefix == 'convnet':
-        # Input
-        print("--- Input ---")
-        path_input = ""
-        if args.input:
-            path_input = args.input[0]
-            if os.path.splitext(path_input)[-1].lower() != ".pt":
-                raise OSError("--input should point to a '.pt' file")
-            if not os.path.isabs(path_input) and 'REVERT_MODELS' in os.environ:
-                path_input = os.path.join(os.environ['REVERT_MODELS'], args.input[0])
-            else:
-                path_input = os.path.join(os.environ['PWD'], args.input[0])
-                print("Loading model from $PWD. The $REVERT_MODELS environment variable is not defined, see --help for more info.")
-            print("Using {} as input".format(path_input))
-            if not os.path.exists(path_input):
-                raise OSError("The file {} doesn't exist".format(path_input))
-        else:
-            print("Model randomly initialized")
+    #--- Input 
+    if args.input: 
+        if os.path.splitext(args.input)[1] == "":
+            args.input = os.path.extsep.join((args.input, "pt"))
+        args.input = (args.input if os.path.isabs(args.input)
+                                 else os.path.join(models_dir, args.input))
+        print(f"> read initial {prefix} state from {args.input}")
+    else: 
+        print(f"> randomly initialize {prefix}")
 
-        # Output
-        print("--- Output ---")
-        if args.output and os.path.isabs(args.output[0]):
-            path_output = args.output[0]
-            path_traces = args.output[0]
-        else:
-            if 'REVERT_MODELS' in os.environ:
-                path_output = os.environ['REVERT_MODELS']
-            else:
-                path_output = os.environ['PWD']
-                print("Saving model from $PWD. The $REVERT_MODELS environment variable is not defined, see --help for more info.")
-            if 'REVERT_LOGS' in os.environ:
-                path_traces = os.environ['REVERT_LOGS']
-            else:
-                path_traces = os.environ['PWD']
-                print("Saving model from $PWD. The $REVERT_LOGS environment variable is not defined, see --help for more info.")
-            if args.output:
-                path_output = os.path.join(path_output, args.output[0])
-                path_traces = os.path.join(path_traces, args.output[0])
-        
-        file_name = generate_filename(path_output)
-        path_output_file = os.path.join(path_output, file_name) + '.pt'
-        path_traces = os.path.join(path_traces, 'runs', file_name)
-        print("Using {} as output file".format(path_output_file))
-        print("Using {} as output traces logs".format(path_traces))
+    #--- Output
+    if args.output: 
+        if os.path.splitext(args.output)[1] == "":
+            args.output = os.path.extsep.join((args.output, "pt"))
+        args.output = (args.output if os.path.isabs(args.output)
+                                 else os.path.join(models_dir, args.output))
+    else: 
+        args.output = generate_filename(models_dir, prefix)
+    print(f"> save final {prefix} state as {args.output}")
 
-        return path_input, path_output_file, path_traces
+    #--- Tensorboard 
+    key = os.path.splitext(os.path.basename(args.output))[0]
+    args.writer = os.path.join(logs_dir, key)
+    print(f"> save tensorboard traces as {args.writer}")
+
+    return args
+
+
+if __name__ == '__main__': 
+    args = read_args(arg_parser())
