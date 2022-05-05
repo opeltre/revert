6e662acd20dde0dd3cef8658f51d4c8d0f56b77b
Adrien Hannon <adrien_hannon@ens.univ-artois.fr>
2022-05-05 13:37:33 +0200

    Working script to train

--- experiments/unshift_flows.py
+++ experiments/unshift_flows.py
@@ -1,22 +1,68 @@
-from revert.models import ConvNet, Module, getModel, getData
-from revert.experemiments import arg_parse, arg_verifier
-
-import json
+from revert.models import ConvNet, Module
+from experiments import arg_parser, read_args
 
 from torch.optim import SGD, Adam
 from torch.optim.lr_scheduler import ExponentialLR
-from torch.utils.tensorboard import SummaryWriter
 
 import torch
 
-# model state 
+from revert.models import ConvNet, Module
+
+from torch.optim import SGD, Adam
+from torch.optim.lr_scheduler import ExponentialLR
+from revert.transforms import shift_all
+from revert.models import ConvNet, Pipe
+
+from torch.utils.data import TensorDataset, DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
+
+import torch 
+
+# ============== Data =============================
+
+def getData(stdev) :
+    data = torch.load("../scripts-pcmri/pcmri_tensor.pt")
+    flows = data["flows"]
+
+    shifted, y = shift_all(stdev)(flows)
+
+    data_dataset = TensorDataset(shifted, y)
+    data_loader = DataLoader(data_dataset, shuffle=True, batch_size=1)
+    
+    return data_loader
+
+
+#==================================================
+
+#--- Models ---
+
+Npts = 32
+layers = [[Npts, 6,   8],
+            [16,  6*12,  8],
+            [8,   6*24,  8],
+            [1,   6*12,  1]]
+
+base = ConvNet(layers, pool='max')
+
+dim_out = 6*12
+dim_task = 6
+head = ConvNet([[1, dim_out, 1], [1, dim_task, 1]]) 
+    
+convnet = Pipe(base, head)
+    
+# find the path to save 
+args  = read_args(arg_parser(prefix='convnet'))
+if args.input :
+    convnet = convnet.load(args.input)
+
 
 #--- Main ---
 
 def main(defaults=None, stdev=0.5):
-    
+        
     if defaults is None : 
-        defaults = {'epochs':  5,
+        defaults = {'epochs':  1,
                     'n_batch': 128,
                     'lr':      1e-3,
                     'gamma':   0.8,
@@ -25,35 +71,26 @@ def main(defaults=None, stdev=0.5):
                     } | (defaults if defaults else {})
     else : 
         defaults = defaults | { 'stdev' : stdev }
-
+        
     # take all the data
     dataLoad = getData(defaults['stdev']) 
-    # generate the model
-    model = getModel()
-    # find the path to save 
-    args = arg_parse()
-    path, name = arg_verifier(args)
         
-    print(model.modules)
-    #--- optimizer ---
-    optim = Adam(model.parameters(), lr=defaults['lr'])
+    # optimizer 
+    optim = Adam(convnet.parameters(), lr=defaults['lr'])
     lr    = ExponentialLR(optim, gamma=defaults['gamma'])
-        
-    model.writer = SummaryWriter(path + "runs/"+ name)
-    
-    model.fit(dataLoad, optim, lr, epochs=defaults['epochs'], w="Loss")
+           
+    convnet.fit(dataLoad, optim, lr, epochs=defaults['epochs'], w="Loss")
     free(optim, lr)
-    
-    # save the hyper parameter to tensorboard
-    for key, value in defaults.items():
-        model.writer.add_text(key , str(value))
-
-    #save the hyper parameter in a json file
-    with open(path + "runs/"+ name, 'w') as js:
-        json.dump(defaults, js)
-    
-    #save the head
-    model.modules[-1].save(path + "models/" + name)
+     
+    if isinstance(convnet.writer, dict) :    
+        # save the hyper parameter to writer dict
+        for key, value in defaults.items():
+            convnet.writer[key] = str(value)
+    elif isinstance(convnet.writer, SummaryWriter) :    
+        # save the hyper parameter to tensorboard
+        for key, value in defaults.items():
+            convnet.writer.add_text(key , str(value))
+        convnet.writer.close()
             
 #--- Cuda free ---
 
@@ -61,24 +98,8 @@ def free (*xs):
     for x in xs: del x
     torch.cuda.empty_cache()
 
-def different_test():
+if __name__ == "__main__":
+    print(f'\convnet:\n {convnet}')
     main()
-    
-    defaults = {'epochs':  5,
-                    'n_batch': 128,
-                    'lr':      1e-3,
-                    'gamma':   0.8,
-                    'n_it':    3750
-                    }
-    main(defaults)
-    
-    defaults = {'epochs':  5,
-                'n_batch': 256,
-                'lr':      1e-3,
-                'gamma':   0.8,
-                'n_it':    3750
-                }
-    main(defaults, 0.5)
-
-
-different_test()
+    convnet.save(args.output)
+    mod = torch.load(args.output)
