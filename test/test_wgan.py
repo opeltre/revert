2ff714cfc0381465926b20fe380087418d68e7f6
Olivier Peltre <opeltre@gmail.com>
2022-05-13 11:19:48 +0200

    add test_wgan_fit
    WGAN.fit method is tested for a 3D gaussian support in 6D space
    
    Skip this test by default (~20s to fit)

--- test/test_wgan.py
+++ test/test_wgan.py
@@ -1,4 +1,5 @@
 import test
+import unittest
 import torch
 
 from revert.models import WGAN, WGANCritic, ConvNet, View
@@ -33,7 +34,8 @@ class TestWGAN(test.TestCase):
         #--- Critic
         self.assertTrue(isinstance(gan1.critic, WGANCritic))
         self.assertTrue(gan1.critic is D1)
-
+        self.assertTrue(len(D1.clipped) > 0)
+    
     def test_wgan_losses(self):
         gan = WGAN(G, View([1]) @ D)
         #--- critic loss:    E[c(x_true)] - E[c(x_gen)]
@@ -42,3 +44,33 @@ class TestWGAN(test.TestCase):
         w_loss = gan.loss_on(z, x_true)
         self.assertTrue(c_loss.dim() == 0)
         self.assertTrue(w_loss.dim() == 0)
+
+    @unittest.skip("optional WGAN fit")
+    def test_wgan_fit(self):
+        N, Nb = 200, 40
+        gan = WGAN(G, D)
+        gan.n_critic = 100
+        #--- input 3d-seeds
+        z = torch.randn([N, Nb, 3, 1])
+        #--- observed 3d-distribution
+        x = .3 + torch.randn([N * Nb, 6])
+        P = torch.tensor([[1, 0, 0, 0, 0, 0],
+                          [0, 1, 0, 0, 0, 0],
+                          [0, 0, 1, 0, 0, 0]]).float()
+        proj = P.T @ P
+        x_true = (proj @ x.T).T.view([N, Nb, 6, 1])
+        #--- backprop 
+        dset =  [(zi, xi) for zi, xi in zip(z, x_true)]
+        gan.fit(dset, lr=1e-3, epochs=1, progress=False)
+        #--- generate
+        with torch.no_grad():
+            x_gen = gan(z.view([-1, 3, 1]))
+        #--- test mean ([.3, .3, .3, 0., 0., 0.])
+        expect = x_gen.mean()
+        result = x_true.mean()
+        self.assertClose(expect, result, tol=.1)
+        #--- test subspace (proj @ x = x)
+        x_gen = x_gen.view([-1, 6])
+        expect = proj @ x_gen.T
+        result = x_gen.T
+        self.assertClose(expect, result, tol=.1)
