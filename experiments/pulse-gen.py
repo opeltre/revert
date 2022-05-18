b448cb8cf8bc54b059b2626682a5284fda6f6f93
Olivier Peltre <opeltre@gmail.com>
2022-05-18 20:02:22 +0200

    update experiments/pulse-gen to cuda

--- experiments/pulse-gen.py
+++ experiments/pulse-gen.py
@@ -13,13 +13,12 @@ dz, dx = 8, 64
 ns = (20, 500)          # (n_gen, n_crit)   : respective number of iterations
 lr = (1e-2, 5e-3)       # (lr_gen, lr_crit) : respective learning rates 
 clip = .6               # critic.clip_value : Lipschitz constraint
-epochs = 20
 n_batch = 256
 
 args = cli.parse_args(dirname='pulse-gen',
                       output='WGAN-8:64',
                       datatype='infusion',
-                      epochs=20,
+                      epochs=50,
                       data='baseline-no_shunt.pt')
 
 def main (args):
@@ -48,29 +47,34 @@ def main (args):
     # (seed, sample) pairs
     pulses = (nn.AvgPool1d(2)(data['pulses'])
                 .view([-1, 64]))
-
+    
     N = (pulses.shape[0] // n_batch) * n_batch
     idx = torch.randperm(N)
 
     x_true = (pulses[:N][idx]
-                .reshape([-1, n_batch, 64]))
+                .reshape([-1, n_batch, 64])
+                .cuda())
 
-    z_gen = torch.randn(N // n_batch, n_batch, 8)
+    z_gen = torch.randn(N // n_batch, n_batch, 8).cuda()
     
     dset = [(zi, xi) for zi, xi in zip(z_gen, x_true)]
     
     def fit ():
         # Optimize WGAN loss
+        print(f"Fitting WGAN on {N} pulses")
         gan.writer = SummaryWriter(args.writer)
+        gan.cuda()
         gan.fit(dset, lr=lr[0], epochs=args.epochs, tag="critic score")
         gan.save(args.output)
 
     @gan.epoch
     def plot_samples(tag, data, epoch):
-        z = torch.randn(12, 8)
+        if epoch % 10 != 9:
+            return "skipped"
+        z = torch.randn(12, 8).cuda()
         with torch.no_grad():
-            x_gen = gan(z)
+            x_gen = gan(z).cpu()
         fig = rp.pulses(x_gen)
-        gan.writer.add_figure(f"Samples at epoch {epoch}", fig)
+        gan.writer.add_figure(f"Samples at epoch {epoch + 1}", fig)
 
     fit()
