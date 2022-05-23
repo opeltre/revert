bdb4b096ff8198c507f73bf69c265a9ae2585f7f
Olivier Peltre <opeltre@gmail.com>
2022-05-23 21:45:34 +0200

    add Saxpy layer + norm='Saxpy'|'BN' conv argument

--- revert/models/linear.py
+++ revert/models/linear.py
@@ -3,6 +3,25 @@ import torch.nn as nn
 
 from .module import Module
 
+class Saxpy(Module):
+
+    def __init__(self, shape, bias=True, stdev=.1):
+        super().__init__()
+        self.shape = shape
+        w = 1 + stdev * torch.randn(shape)
+        b = stdev * torch.randn(shape)
+        self.register_parameter('weight', nn.Parameter(w))
+        self.register_parameter('bias', nn.Parameter(b))
+
+    @torch.no_grad()
+    def set(self, weight, bias):
+        self.weight = nn.Parameter(weight)
+        self.bias = nn.Parameter(bias)
+
+    def forward(self, x):
+        return self.weight * x + self.bias
+
+
 class Affine(Module):
 
     def __init__(self, d_in, d_out, dim=-1, bias=True):
@@ -13,7 +32,7 @@ class Affine(Module):
         self.dim = dim
         self.weight = self.module.weight
         self.bias   = self.module.bias
-    
+
     @torch.no_grad()
     def set(self, weight, bias=None):
         self.weight = nn.Parameter(weight)
@@ -34,7 +53,7 @@ class Affine(Module):
             y = y.reshape([*ns[:-1], self.d_out])
             return y if self.dim == -1 else y.transpose(-1, self.dim)
         raise TypeError(f"Invalid shape {x.shape} for d_in={self.d_in}")
-    
+
     def __repr__(self):
         return f'Affine({self.d_in}, {self.d_out}, dim={self.dim})'
 
@@ -42,6 +61,6 @@ class Linear(Affine):
 
     def __init__(self, d_in, d_out, dim=-1):
         super().__init__(d_in, d_out, dim, False)
-        
+
     def __repr__(self):
         return f'Linear({self.d_in}, {self.d_out}, dim={self.dim})'
